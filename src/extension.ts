9e7958f1f3f6dbc48808f16554e0b765129deec0
taimast <charlskenno@gmail.com>
2025-03-21 22:25:54 +0300

    Refactor gitleaks extension commands and improve diagnostics handling

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,13 @@ import * as os from 'os';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
+const diagnosticCollection = vscode.languages.createDiagnosticCollection('gitleaks');
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "gitleak-vs" is now active!');
 
 	context.subscriptions.push(
 		vscode.workspace.onDidSaveTextDocument(doc => {
-
 			const homeDir = os.homedir();
 			const leaksDir = path.join(homeDir, '.gitleaks-vs');
 			const leaksFilePath = path.join(leaksDir, 'leaks.json');
@@ -20,11 +21,8 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			// Вызываем gitleaks и выводим результаты в .gitleaks-vs/leaks.json
 			exec(`gitleaks dir -r ${leaksFilePath} ` + doc.uri.fsPath, (error, stdout, stderr) => {
-
 				if (error) {
 					if (error.message.includes('leaks found: ')) {
-						// После выполнения gitleaks, парсим leaks.json
-						highlightSecrets(doc, []); // Очищаем подсветку
 						parseGitleaksOutput(doc, leaksFilePath);
 						return;
 					} else {
@@ -32,10 +30,8 @@ export function activate(context: vscode.ExtensionContext) {
 						return;
 					}
 				} else {
-					highlightSecrets(doc, []); // Очищаем подсветку
-
+					clearDiagnostics(doc);
 				}
-
 			});
 		})
 	);
@@ -51,7 +47,7 @@ function parseGitleaksOutput(doc: vscode.TextDocument, leaksFilePath: string) {
 
 		try {
 			const leaks = JSON.parse(data); // Парсим JSON-данные
-			const secretRanges: vscode.Range[] = [];
+			const diagnostics: vscode.Diagnostic[] = [];
 
 			// Обходим каждый объект в массиве leaks
 			leaks.forEach((leak: any) => {
@@ -59,28 +55,33 @@ function parseGitleaksOutput(doc: vscode.TextDocument, leaksFilePath: string) {
 				const endLine = leak.EndLine - 1; // zero-based index
 				const startPos = new vscode.Position(startLine, leak.StartColumn - 1);
 				const endPos = new vscode.Position(endLine, leak.EndColumn);
-				secretRanges.push(new vscode.Range(startPos, endPos));
+
+				const range = new vscode.Range(startPos, endPos);
+				const diagnostic = new vscode.Diagnostic(
+					range,
+					`Potential secret exposure found: ${leak.Message}`,
+					vscode.DiagnosticSeverity.Warning
+				);
+
+				diagnostics.push(diagnostic);
 			});
 
-			// Добавление декоратора для выделения секретов
-			highlightSecrets(doc, secretRanges);
+			// Обновление диагностики в редакторе
+			updateDiagnostics(doc.uri, diagnostics);
 		} catch (parseError) {
 			console.error(`Error parsing ${leaksFilePath}: ${parseError}`);
 		}
 	});
 }
 
-function highlightSecrets(doc: vscode.TextDocument, secretRanges: vscode.Range[]) {
-	// Декоратор для подсветки
-	const decorationType = vscode.window.createTextEditorDecorationType({
-		backgroundColor: 'rgba(255, 0, 0, 0.3)', // Красный фон
-		border: '1px solid red' // Красная рамка
-	});
-
-	const editor = vscode.window.activeTextEditor;
-	if (editor && editor.document === doc) {
-		editor.setDecorations(decorationType, secretRanges);
-	}
+function updateDiagnostics(uri: vscode.Uri, diagnostics: vscode.Diagnostic[]) {
+	diagnosticCollection.set(uri, diagnostics);
 }
 
-export function deactivate() { }
+function clearDiagnostics(doc: vscode.TextDocument) {
+	diagnosticCollection.set(doc.uri, []);
+}
+
+export function deactivate() {
+	diagnosticCollection.dispose();
+}
